037780552985df64b3abe3747eb728cfc9d81dcb
andyahn <andyahn1991@gmail.com>
2021-07-16 15:27:06 +0900

    thr

--- ptp_test_case1.py
+++ ptp_test_case1.py
@@ -1,10 +1,9 @@
-# case 2
+# case 1
 import re
-import skt_ptp_test as ptp
+import ptp_test as ptp
 
-#host = '172.100.100.218'; username = 'admin'; password = 'admin'; port = '3/x1/1/c1/1'
 host, username, password = ptp.check_host(True); platform = 'alcatel_sros'; check_port = re.compile('^(\d{1,2})[/](\w{1,3})[/](\w{1,3})'); check_asy = re.compile('\d{5,}')
 device = ptp.connect(platform, host, username, password); port, ptp_asy = ptp.inputs(check_port, check_asy)
-delay_time = 200; loop_count = 16; print_s = f'\nwindow (2초 간격)으로 계산되는 delay 를 PTSF 미 발동 조건인 250 ns 미만 변동 ({delay_time}ns)'; loop_bool = ptp.call(device, check_asy, port, ptp_asy, print_s, delay_time, loop_count)
+delay_time = 260; loop_count = 16; print_s = f'\nwindow (2초 간격)으로 계산되는 delay 를 PTSF 발동 조건인 250 ns 이상 변동 ({delay_time}ns)'; loop_bool = ptp.call(device, check_asy, port, ptp_asy, print_s, delay_time, loop_count)
 
 ptp.after_check(loop_bool, device, check_port, check_asy, port, ptp_asy, print_s, delay_time, loop_count, platform)
